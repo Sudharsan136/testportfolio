4df50a12a84ad30b5ce9bd116b20dacb77159353
sudharsan136 <sudharsan1364@gmail.com>
2026-03-05 10:43:13 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ function App() {
   return (
     <div>
       <div className='flex justify-center w-full'>
-        <div >
-          <h1 className='text-red-800 text-xl font-bold'><span className='text-blue-400 font-extrabold text-5xl relative'>H</span>ello Welcome To My Page</h1>
+        <div className='' >
+          <h1 className='text-red-800 text-xl font-bold animate-slideDown '><span className='text-blue-400 font-extrabold text-5xl relative'>H</span>ello Welcome To My Page</h1>
           <div className='absolute right-5 top-3'>
-            <h2 className='text-xl font-bold'><FontAwesomeIcon icon={faEnvelope} className="text-black text-xl hover:text-gray-500 cursor-pointer mr-2" />sudharsanfs13@gmail.com</h2>
-            <h3 className='text-xl font-semibold'><FontAwesomeIcon icon={faPhone} className="text-black text-xl hover:text-gray-500 cursor-pointer mr-2" />+91 8248373770</h3>
+            <h2 className='text-xl font-bold animate-slideDown'><FontAwesomeIcon icon={faEnvelope} className="text-black text-xl hover:text-gray-500 cursor-pointer mr-2" />sudharsanfs13@gmail.com</h2>
+            <h3 className='text-xl font-semibold animate-slideDown'><FontAwesomeIcon icon={faPhone} className="text-black text-xl hover:text-gray-500 cursor-pointer mr-2" />+91 8248373770</h3>
           </div>
            
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // Define the movement here
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      // Create the shortcut name
+      animation: {
+        slideDown: 'slideDown 100s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
